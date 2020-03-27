e7bea2a5c1cf7cfb708e774943d799b961ca5266
Shawn <shawn.harris3510@gmail.com>
2020-03-27 11:49:21 -0400

    navbar component successfully showing

--- ux-portfolio/src/app/app.module.ts
+++ ux-portfolio/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './homeComponents/navbar/navbar.component';
 
 @NgModule({
   declarations: [
