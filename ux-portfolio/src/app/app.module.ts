de67541e9fe81f7b6ae0a6c0c79a8846994a51fe
Shawn <shawn.harris3510@gmail.com>
2020-03-27 12:24:42 -0400

    footer component successfully displaying

--- ux-portfolio/src/app/app.module.ts
+++ ux-portfolio/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './homeComponents/navbar/navbar.component';
+import { FooterComponent } from './homeComponents/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
