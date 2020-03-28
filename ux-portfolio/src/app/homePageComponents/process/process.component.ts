e103340db89b2c40520f2f4de9d31a6033c32dde
Shawn <shawn.harris3510@gmail.com>
2020-03-28 12:46:05 -0400

    process component successfully showing

---
+++ ux-portfolio/src/app/homePageComponents/process/process.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-process',
+  templateUrl: './process.component.html',
+  styleUrls: ['./process.component.sass']
+})
+export class ProcessComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
