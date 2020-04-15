f0679c3bcee27d25ae3597920fb36df4191a6992
Shawn <shawn.harris3510@gmail.com>
2020-04-14 22:01:27 -0400

    homeDetail routing working

--- ux-portfolio/src/app/app-routing.module.ts
+++ ux-portfolio/src/app/app-routing.module.ts
@@ -1,10 +1,60 @@
-import { NgModule } from '@angular/core';
+// import { NgModule } from '@angular/core';
+// import { Routes, RouterModule } from '@angular/router';
+
+// const routes: Routes = [];
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
+
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeDetailsComponent } from './homePageComponents/home-details/home-details.component'
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+const routes: Routes = [
+    {
+        path: '',
+        component: HomeDetailsComponent,
+    },
+    // {
+    //     path: 'jkRowling',
+    //     component: JkRowlingComponent,
+    // },
+    // {
+    //     path: 'books',
+    //     component: BooksComponent
+    // },
+    // {
+    //     path: 'movies',
+    //     component: MoviesComponent
+    // },
+    // {
+    //     path: 'dining',
+    //     component: DiningComponent
+    // },
+    // {
+    //     path: 'rides',
+    //     component: RidesComponent
+    // },
+    // {
+    //     path: 'shops',
+    //     component: ShopsComponent
+    // },
+    // {
+    //     path: 'shows',
+    //     component: ShowsComponent
+    // },
+
+    // // REMOVE WHEN CLEAR
+    // {
+    //     path: 'quotes',
+    //     component: QuotesComponent
+    // },
+
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
+];
+
+export const AppRoutingModule = RouterModule.forRoot(routes);
